5ba628a23f0346e96c8b8329cbe8ce0aded7f68a
Tannistha Mishra <tannisthamishra460@gmail.com>
2026-06-24 20:59:06 +0530

    Fix app module path for Streamlit Cloud

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -1,6 +1,17 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            ".."
+        )
+    )
+)
+
 import streamlit as st
 import tempfile
-import os
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
